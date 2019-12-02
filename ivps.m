4e7b592a1ea768ce907ec4653a8808c485adafa4
Cecijvp <cecijvp.3@gmail.com>
2019-12-01 19:19:45 -0600

    ultimos ejercicios y modificaciones

--- ivps.m
+++ ivps.m
@@ -13,18 +13,16 @@ function [t,y] = ivps(f,t0,y0,h,tf,metodo)
             solver = @trapezoide; 
             
         case 'rk4'
-            solver = @trapezoide; 
+            solver = @rk4; 
             
         otherwise
             error('Método desconocido');
     end
     
-    t= t0:h:tf;
-    n = length(t);
-    y= zeros(length(y0),n);
-    
-    y(:,1)= y0;
-    for i=1:1:n-1
+    t = t0:h:tf;
+    y = zeros(length(y0),length(t));
+    y(:,1) = y0;
+    for i=1:1:length(t)-1
         y(:,i+1) = y(:,i) + solver(f,t(i),y(:,i),h)*h; 
     end
 
@@ -47,24 +45,10 @@ function phi= trapezoide(f,t,y,h)
         phi =(s1+s2)/2;     
 end
 
-function phi= rk4(f,t,y,h)
-% Runge-Kutta method
-% [a, b] intervalo de la función 
-%y0 condición inicial 
-% n = número de subintervalos 
-%f = función dependiente de y ,t
-h = (b-a)/n;
-halfh = h/2;
-y(1,:)= y0;
-t(1)= a;
-h6 = h/6;
-for i = 1 : n
-    t(i+1) = t(i) + h;
-    th2 = t(i) + halfh;
-    s1 = f(t(i), y(i,:));
-    s2 = f(th2, y(i,:) + halfh * s1);
-    s3 = f(th2, y(i,:) + halfh * s2);
-    s4 = f(t(i+1), y(i,:) + h * s3);
-    y(i+1,:) = y(i,:) + (s1 + s2+s2 + s3+s3 + s4) * h6;
-end
+function phi = rk4(f,t,y,h)
+        s1 = f(t,y);
+        s2 = f(t+h/2, y + (h/2)*s1);
+        s3 = f(t+h/2, y + (h/2)*s2);
+        s4 = f(t+h, y + h*s3);
+        phi = (s1 + 2*s2 + 2*s3 + s4)/6;
 end
