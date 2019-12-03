8a8c08d2d2e55b41a8601d9a03261a995a849bc8
Cecijvp <cecijvp.3@gmail.com>
2019-12-03 10:27:35 -0600

    proyecto final

---
+++ ProyectoFinal/ivps.m
@@ -0,0 +1,54 @@
+function [t,y] = ivps(f,t0,y0,h,tf,metodo)
+ %solucionador de problemas con valor inicial
+ % y' = f(t,y)   y(0)=y0
+ 
+    switch metodo
+        case 'euler'
+            solver = @euler; 
+            
+        case 'midpoint'
+            solver = @midpoint;
+            
+        case 'trapezoide'
+            solver = @trapezoide; 
+            
+        case 'rk4'
+            solver = @rk4; 
+            
+        otherwise
+            error('Método desconocido');
+    end
+    
+    t = t0:h:tf;
+    y = zeros(length(y0),length(t));
+    y(:,1) = y0;
+    for i=1:1:length(t)-1
+        y(:,i+1) = y(:,i) + solver(f,t(i),y(:,i),h)*h; 
+    end
+
+end
+
+function phi= euler(f,t,y,~)
+    phi = f(t,y);
+end
+
+function phi= midpoint(f,t,y,h)
+    s1=f(t,y);
+    ym= y + s1*(h/2);
+    phi=f(t+h/2,ym);
+end
+
+function phi= trapezoide(f,t,y,h)
+        s1=f(t,y);
+        yf=y+h*s1;
+        s2=f(t+h,yf);
+        phi =(s1+s2)/2;     
+end
+
+function phi = rk4(f,t,y,h)
+        s1 = f(t,y);
+        s2 = f(t+h/2, y + (h/2)*s1);
+        s3 = f(t+h/2, y + (h/2)*s2);
+        s4 = f(t+h, y + h*s3);
+        phi = (s1 + 2*s2 + 2*s3 + s4)/6;
+end
